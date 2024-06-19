db63c9207a1b96175739b035cb660fd5cad8574f
AdletMier <eypk777@gmail.com>
2024-06-19 23:29:10 +0900

    Remove UnAvaliable Classes, Add Gameplay Ability System in PlayerState, Try Adding Gameplay Ability

--- Source/JEXPR/JEXPR.Build.cs
+++ Source/JEXPR/JEXPR.Build.cs
@@ -8,6 +8,6 @@ public class JEXPR : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 	}
 }
